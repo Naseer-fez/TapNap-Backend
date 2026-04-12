91e10e78a6f072869d728758eda411defd51f9fc
Naseer <feznaseer777@gmail.com>
2026-04-12 12:23:25 +0530

    Chnaged a Critial Bug

--- models/LinksTable.py
+++ models/LinksTable.py
@@ -5,7 +5,7 @@ import time
 class Links(db.Model):
     __tablename__="Links"
     ID = db.Column(db.Integer, primary_key=True, autoincrement=True)    
-    UserId=db.Column(db.Integer, db.ForeignKey('User.id',ondelete='SET NULL'),index=True, nullable=True,default=-1)
+    UserId=db.Column(db.Integer, db.ForeignKey('User.id',ondelete='SET NULL'),index=True, nullable=True,default=None)
     Link=db.Column(db.String(768),unique=False,nullable=False)
     Password=db.Column(db.String(256),nullable=True)
     Code=db.Column(db.Integer,unique=True,nullable=False)
