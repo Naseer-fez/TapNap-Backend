8f5f5241355c34d31abc65996c98fb3e10967c63
Naseer <feznaseer777@gmail.com>
2026-04-13 13:55:41 +0530

    Improved the Api end piont

--- routes/Forgot/Email.py
+++ routes/Forgot/Email.py
@@ -6,21 +6,26 @@ FrgEmail=Blueprint("Forgot",__name__)
 
 
 
-@FrgEmail.route("/Forgot/<String:email>",methods=["POST"])
+@FrgEmail.route("/Forgot/<string:email>",methods=["POST","GET"])
 @RequiredRateLimiter()
 def ForgotEmail(email):
-    code=OTP(email=email) ##need to send the email
-    Data=STORAGE(email=email,otp=code,)
-    return jsonify(Data[0]),Data[1]
-
-@FrgEmail.route("/Verify/<String:email>",methods=["GET"])
-@RequiredRateLimiter()
-def Emailcheck(email):
-    Data=request.get_json()
-    Code=Data.get("code")
-    info=STORAGE(email=email,otp=Code,action="Check")
-    if(info[1])==400:
+    if request.method=="POST":
+        code=OTP(email=email) ##need to send the email
+        if code =='E':
+            return jsonify({"Report":code}),400
+        Data=STORAGE(email=email,otp=code) ## To store the data in memory 
+        return jsonify(Data[0]),Data[1]
+    else:
+        # Data=request.get_json()
+        Code=request.headers.get('X-OTP-Code')
+        info=STORAGE(email=email,otp=Code,action="Check")
+        if(info[1])==400:
+            return jsonify(info[0]),info[1]
         return jsonify(info[0]),info[1]
-    # Now give the access
-    #I need this to chnage to createa branch 
+
+# @FrgEmail.route("/Verify/<string:email>",methods=["GET"])
+# @RequiredRateLimiter()
+# def Emailcheck(email):
+#     
+
     
